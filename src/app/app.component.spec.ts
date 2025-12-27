bd26e52b181d6cb48bcfdf11d41e0c4425b082f8
c4zuko <101098340+c4zuko@users.noreply.github.com>
2025-12-28 00:15:39 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cron_angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Cron Expression Visualizer');
   });
 });
+
